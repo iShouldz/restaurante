61c994272ff317368ad869fa59ecd642f6be9186
Pedro Souza <pedrosport10@gmail.com>
2024-03-21 13:28:09 -0300

    FEAT: Começando estrutura para o menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import styles from './menu.module.css'
+
+const Menu = () => {
+  return (
+    <section className={styles.menuContainer}>
+        <h2>Conheça nosso menu</h2>
+    </section>
+  )
+}
+
+export default Menu

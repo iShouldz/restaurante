3d4e3056d2f216c78e0953c02fa88c0145efce43
Pedro Souza <pedrosport10@gmail.com>
2024-03-21 14:42:53 -0300

    FEAT: Estruturando melhor o site e criando um objeto para melhor gerenciamento dos itens

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,34 +1,22 @@
 import ItemMenu from "../ItemMenu/ItemMenu";
 import styles from "./menu.module.css";
-import hamburguer1 from "../../assets/imagens/hamb-1.png";
-
+import cart from "../../assets/shopping-cart.svg";
+import { itensCart } from "../../utils/itens";
 const Menu = () => {
   return (
     <section className={styles.menuContainer}>
       <h2>Conheça nosso menu</h2>
 
       <article className={styles.itensContainer}>
-        <ItemMenu
-          img={hamburguer1}
-          titulo="Cheese Burger Duplo"
-          valor={50.20}
-          descricao="Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa"
-        />
-        <ItemMenu
-          img={hamburguer1}
-          titulo="Cheese Burger Duplo"
-          descricao="Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa"
-        />
-        <ItemMenu
-          img={hamburguer1}
-          titulo="Cheese Burger Duplo"
-          descricao="Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa"
-        />
-        <ItemMenu
-          img={hamburguer1}
-          titulo="Cheese Burger Duplo"
-          descricao="Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa"
-        />
+        {itensCart.map((item) => (
+          <ItemMenu
+            key={item.img}
+            img={item.img}
+            titulo={item.title}
+            valor={item.valor}
+            descricao={item.descricao}
+          />
+        ))}
       </article>
     </section>
   );
---
+++ src/utils/itens.js
@@ -0,0 +1,55 @@
+import hamburguer1 from '../assets/imagens/hamb-1.png'
+import hamburguer2 from '../assets/imagens/hamb-2.png'
+import hamburguer3 from '../assets/imagens/hamb-3.png'
+import hamburguer4 from '../assets/imagens/hamb-4.png'
+import hamburguer5 from '../assets/imagens/hamb-5.png'
+import hamburguer6 from '../assets/imagens/hamb-6.png'
+import hamburguer7 from '../assets/imagens/hamb-7.png'
+import hamburguer8 from '../assets/imagens/hamb-8.png'
+
+
+export const itensCart = [
+    {
+        img: hamburguer1,
+        title: 'Cheese Burger Duplo',
+        descricao: 'Pão levinho de fermentação natural da Trigou, burger 160g, queijo prato e maionese da casa',
+        valor: '35.00'
+    },
+    {
+        img: hamburguer2,
+        title: 'Hambúrguer Clássico',
+        descricao: 'Pão de hambúrguer com um suculento hambúrguer de carne, queijo cheddar, alface, tomate, cebola roxa e molho especial.',
+        valor: '15.00'
+    }, {
+        img: hamburguer3,
+        title: 'Hambúrguer Vegetariano',
+        descricao: 'Pão de hambúrguer integral com um hambúrguer de quinoa e feijão preto, queijo suíço, rúcula, tomate, cebola caramelizada e maionese de ervas.',
+        valor: '14.00'
+    }, {
+        img: hamburguer4,
+        title: 'Hambúrguer BBQ',
+        descricao: 'Pão de hambúrguer com um hambúrguer de carne angus, bacon crocante, queijo gorgonzola, cebola frita, alface e molho barbecue.',
+        valor: '17.00'
+    }, {
+        img: hamburguer5,
+        title: 'Hambúrguer Frango Grelhado',
+        descricao: 'Pão de hambúrguer de batata doce com um peito de frango grelhado, queijo suíço, abacate, rúcula, tomate e maionese de chipotle.',
+        valor: '16.00'
+    }, {
+        img: hamburguer6,
+        title: 'Hambúrguer Duplo Bacon',
+        descricao: 'Dois hambúrgueres de carne, queijo cheddar, fatias de bacon crocante, alface, tomate, cebola roxa e maionese de alho.',
+        valor: '18.00'
+    }, {
+        img: hamburguer7,
+        title: 'Hambúrguer Tex-Mex',
+        descricao: 'Pão de hambúrguer de milho com um hambúrguer de carne temperado, guacamole, pico de gallo, queijo pepper jack, jalapeños e nachos crocantes.',
+        valor: '16.00'
+    },
+    {
+        img: hamburguer8,
+        title: 'Hambúrguer Vegano',
+        descricao: 'Pão de hambúrguer de espinafre com um hambúrguer de grão de bico e lentilha, queijo vegano, folhas de espinafre, tomate, cebola roxa e maionese vegana.',
+        valor: '14.00'
+    },
+]
